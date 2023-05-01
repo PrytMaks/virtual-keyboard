caca960941869a6e057dc08904831225b63af8a5
PrytMaks <prytkovmaks@gmail.com>
2023-05-02 00:26:46 +0500

    feat: test styles

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -1,3 +1,11 @@
-class Keyboard {
-  
-}
+import css from './style.css';
+
+console.log('Hello');
+
+let title = document.createElement('div');
+title.classList.add('text')
+title.innerHTML = 'HELLO WORLD';
+
+let body = document.querySelector('body');
+console.log(body);
+body.append(title);
